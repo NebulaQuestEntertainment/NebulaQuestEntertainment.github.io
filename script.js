686ce66511d5508790177fc391a8f07afcb96033
NebulaQuestEntertainment <150523579+NebulaQuestEntertainment@users.noreply.github.com>
2023-11-11 15:35:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
 // script.js
 
-function showTab(tabId) {
-    // Hide all sections
-    var sections = document.querySelectorAll('.section');
-    sections.forEach(function (section) {
-        section.classList.remove('active');
+function scrollToSection(tabId) {
+    var targetSection = document.getElementById(tabId);
+
+    if (targetSection) {
+        // Scroll to the target section smoothly
+        targetSection.scrollIntoView({
+            behavior: 'smooth'
+        });
+
+        // Highlight the active tab
+        var tabs = document.querySelectorAll('.tab');
+        tabs.forEach(function (tab) {
+            tab.classList.remove('active');
+        });
+
+        var selectedTab = document.querySelector('.tab[data-tab="' + tabId + '"]');
+        if (selectedTab) {
+            selectedTab.classList.add('active');
+        }
+    }
+}
+
+// Add an event listener for scrolling
+window.addEventListener('scroll', function () {
+    var currentSection = null;
+
+    // Find the section currently in view
+    document.querySelectorAll('.section').forEach(function (section) {
+        var rect = section.getBoundingClientRect();
+        if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
+            currentSection = section.id;
+        }
     });
 
-    // Deactivate all tabs
+    // Highlight the active tab based on the current section
     var tabs = document.querySelectorAll('.tab');
     tabs.forEach(function (tab) {
         tab.classList.remove('active');
     });
 
-    // Show the selected section
-    var selectedSection = document.getElementById(tabId);
-    if (selectedSection) {
-        selectedSection.classList.add('active');
+    var activeTab = document.querySelector('.tab[data-tab="' + currentSection + '"]');
+    if (activeTab) {
+        activeTab.classList.add('active');
     }
-
-    // Activate the corresponding tab
-    var selectedTab = document.querySelector('.tab[data-tab="' + tabId + '"]');
-    if (selectedTab) {
-        selectedTab.classList.add('active');
-    }
-}
+});
