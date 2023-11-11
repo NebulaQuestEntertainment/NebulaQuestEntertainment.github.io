b36a96e6bdc4f0bd58b9d2255dd0e3706ce1dca1
NebulaQuestEntertainment <150523579+NebulaQuestEntertainment@users.noreply.github.com>
2023-11-11 15:32:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,21 @@ function showTab(tabId) {
         section.classList.remove('active');
     });
 
+    // Deactivate all tabs
+    var tabs = document.querySelectorAll('.tab');
+    tabs.forEach(function (tab) {
+        tab.classList.remove('active');
+    });
+
     // Show the selected section
     var selectedSection = document.getElementById(tabId);
     if (selectedSection) {
         selectedSection.classList.add('active');
+    }
+
+    // Activate the corresponding tab
+    var selectedTab = document.querySelector('.tab[data-tab="' + tabId + '"]');
+    if (selectedTab) {
+        selectedTab.classList.add('active');
     }
 }
